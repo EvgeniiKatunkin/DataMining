de5cf7c8cae37a8a5e3a5f1810cbf90ecae1a2a2
EvgeniiKatunkin <evgeniikatunkin@gmail.com>
2021-03-19 15:59:58 +0500

    Squares were transformed to cubes

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 
-x_values = list(range(1, 101))
-y_values = [x**2 for x in x_values]
+x_values = list(range(1, 5001))
+y_values = [x**3 for x in x_values]
 
-plt.scatter(x_values, y_values, s=40)
+plt.scatter(x_values, y_values, c='red',
+            edgecolors='none', s=40)
 
 # Set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
@@ -14,6 +15,6 @@ plt.ylabel("Square of Value", fontsize=14)
 plt.tick_params(axis='both', which='major', labelsize=14)
 
 # Set the range for each axis.
-plt.axis([0, 110, 0, 11000])
+plt.axis([0, 5100, 0, 1000000000000])
 
 plt.show()
