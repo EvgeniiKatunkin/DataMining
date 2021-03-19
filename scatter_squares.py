c6e9b6e02f18ab68a8fa4c253551ae6f4dd2ab7d
EvgeniiKatunkin <evgeniikatunkin@gmail.com>
2021-03-19 16:06:23 +0500

    Squares were transformed to cubes

--- scatter_squares.py
+++ scatter_squares.py
@@ -7,12 +7,12 @@ plt.scatter(x_values, y_values, c='red',
             edgecolors='none', s=40)
 
 # Set chart title and label axes.
-plt.title("Square Numbers", fontsize=24)
+plt.title("Cube Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
-plt.ylabel("Square of Value", fontsize=14)
+plt.ylabel("Cube of Value", fontsize=14)
 
 # Set size of tick labels.
-plt.tick_params(axis='both', which='major', labelsize=14)
+plt.tick_params(axis='both', labelsize=14)
 
 # Set the range for each axis.
 plt.axis([0, 5100, 0, 1000000000000])
